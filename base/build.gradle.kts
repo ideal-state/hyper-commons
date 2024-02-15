05e430bf681cf89740983e1c2b17d3fb81b0598d
ketikai <ketikai@idealstate.team>
2024-02-16 06:34:45 +0800

    build(新增发布与签名脚本): 为手动发布到 maven 中央仓库做准备
    Signed-off-by: ketikai <ketikai@idealstate.team>

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -3,6 +3,8 @@ import java.util.*
 
 plugins {
     id("com.github.johnrengelman.shadow") version ("8.+")
+    id("maven-publish")
+    id("signing")
 }
 
 val projectName = project.ext["projectName"] as String
@@ -41,18 +43,57 @@ tasks.processResources {
     }
 }
 
+val manifestAttributes: MutableMap<String, *> = linkedMapOf(
+        "Group" to project.group,
+        "Name" to projectName,
+        "Version" to project.version,
+        "Authors" to authors,
+        "Updated" to DateFormatUtils.format(Date(), "yyyy-MM-dd HH:mm:ssZ"),
+        "Multi-Release" to true,
+)
+
+tasks.register<Jar>("sourceJar") {
+    archiveBaseName.set(projectName)
+    archiveClassifier.set("source")
+    from(sourceSets.main.get().allSource)
+    manifest {
+        attributes(manifestAttributes)
+    }
+}
+val sourceJar = tasks.named<Jar>("sourceJar")
+
+tasks.javadoc {
+    options {
+        this as StandardJavadocDocletOptions
+        charSet(charset)
+        encoding(charset)
+        docEncoding(charset)
+        locale("zh_CN")
+        windowTitle("${projectName}-${project.version} API")
+        docTitle(windowTitle)
+        author(true)
+        version(true)
+        jFlags("-D'file.encoding'=${charset}")
+    }
+}
+
+tasks.register<Jar>("javadocJar") {
+    dependsOn(tasks.javadoc)
+    archiveBaseName.set(projectName)
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc)
+    manifest {
+        attributes(manifestAttributes)
+    }
+}
+val javadocJar = tasks.named<Jar>("javadocJar")
+
 tasks.shadowJar {
+    dependsOn(sourceJar, javadocJar)
     archiveBaseName.set(projectName)
     archiveClassifier.set("")
     manifest {
-        attributes(linkedMapOf(
-                "Group" to project.group,
-                "Name" to projectName,
-                "Version" to project.version,
-                "Authors" to authors,
-                "Updated" to DateFormatUtils.format(Date(), "yyyy-MM-dd HH:mm:ssZ"),
-                "Multi-Release" to true,
-        ))
+        attributes(manifestAttributes)
     }
 }
 
@@ -63,6 +104,58 @@ tasks.jar {
 }
 
 tasks.create<Copy>("copyToRootBuildLibs") {
-    from(tasks.shadowJar)
+    from(sourceJar, javadocJar, tasks.shadowJar)
     into("${rootProject.projectDir}/build/libs")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = projectName
+            version = project.version.toString()
+
+            pom {
+                name.set(projectName)
+                description.set("提供一些基础的公共工具类")
+                packaging = "jar"
+                url.set("https://github.com/ideal-state/hyper-commons")
+                inceptionYear.set("2024")
+
+                organization {
+                    name.set("ideal-state")
+                    url.set("https://github.com/ideal-state")
+                }
+
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/ideal-state/hyper-commons")
+                    tag.set(version)
+                    connection.set("scm:git:git@github.com:ideal-state/hyper-commons.git")
+                    developerConnection.set("scm:git:git@github.com:ideal-state/hyper-commons.git")
+                }
+            }
+
+            artifact(sourceJar)
+            artifact(javadocJar)
+            artifact(tasks.shadowJar)
+        }
+    }
+    repositories {
+        maven {
+            name = "local"
+            url = uri("file://${projectDir}/build/repository")
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
